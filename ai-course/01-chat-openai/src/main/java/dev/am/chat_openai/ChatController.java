4da82f9ac8922610ceda23e00df12c646de396f3
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-03-02 21:47:24 -0400

    Adding initial commit for ai course related to openai

---
+++ ai-course/01-chat-openai/src/main/java/dev/am/chat_openai/ChatController.java
@@ -0,0 +1,30 @@
+package dev.am.chat_openai;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
+class ChatController {
+    private final ChatClient chatClient;
+
+    ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @PostMapping
+    Output chat(@RequestBody @Valid Input input) {
+        String response = chatClient.prompt(input.prompt())
+                .call()
+                .content();
+        return new Output(response);
+    }
+
+    record Input(@NotBlank String prompt) {}
+    record Output(String response) {}
+}
